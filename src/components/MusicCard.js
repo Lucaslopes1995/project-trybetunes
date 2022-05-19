859463b3ccbee83f6efabe921b92859197d87387
Lucas <lucaslopes@gea.inatel.br>
2022-05-19 01:39:25 -0300

    requisito 9

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 import Carregando from './Carregando';
 
 class MusicCard extends React.Component {
   constructor() {
     super();
-    this.state = { getRespAPI: false };
+    this.state = { mussicFav: false, getRespAPI: false };
     this.handleChange = this.handleChange.bind(this);
   }
 
+  componentDidMount() {
+    const { mussicFav } = this.props;
+    this.setState({ mussicFav });
+  }
+
   async handleChange() {
-    const { music: { trackName } } = this.props;
+    const { music: { trackId } } = this.props;
     // console.log(target.name,target.checked)
     // this.setState({[target.name]:target.checked})
-    const favSong = await addSong(trackName);
+    const favSong = await addSong(trackId);
     console.log(favSong);
     this.setState({ getRespAPI: favSong !== 'OK' });
   }
 
-  async favNewSong(musicName) {
+  async favNewSong() {
+    const { music } = this.props;
+    const { mussicFav } = this.state;
     this.setState({ getRespAPI: true });
-    const favSong = await addSong(musicName);
-    this.setState({ getRespAPI: favSong !== 'OK' });
+    let favSong;
+    if (!mussicFav) {
+      favSong = await addSong(music);
+    } else {
+      // console.log(trackId)
+      favSong = await removeSong(music);
+    }
+
+    // console.log(favSong);
+    this.setState({ getRespAPI: favSong !== 'OK', mussicFav: !mussicFav });
   }
 
   render() {
     const { music } = this.props;
-    const { getRespAPI } = this.state;
-    const { trackName, previewUrl, trackId } = music;
-    //   console.log(music)
+    const { getRespAPI, mussicFav } = this.state;
+    const { trackId, previewUrl, trackName } = music;
+    // console.log(mussicFav)
     return (
       <>
         <p>{trackName}</p>
@@ -41,9 +56,11 @@ class MusicCard extends React.Component {
           <track kind="captions" />
         </audio>
         <input
+          name="mussicFav"
           type="checkbox"
+          checked={ mussicFav }
           data-testid={ `checkbox-music-${trackId}` }
-          onChange={ () => this.favNewSong() }
+          onChange={ (e) => this.favNewSong(e) }
         />
         {getRespAPI && <Carregando />}
       </>
@@ -53,6 +70,7 @@ class MusicCard extends React.Component {
 
 MusicCard.propTypes = {
   music: PropTypes.string.isRequired,
+  mussicFav: PropTypes.bool.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -4,24 +4,26 @@ import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import Carregando from '../components/Carregando';
 import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   constructor() {
     super();
-    this.state = { musics: [], getRespAPI: false };
+    this.state = { musics: [], getRespAPI: false, favSongs: [] };
   }
 
   async componentDidMount() {
+    const favSongs = await getFavoriteSongs();
     const { match: { params: { id } } } = this.props;
     const musics = await getMusics(id);
-    this.setState({ musics });
-    // console.log(musics[0].artistName);
+    this.setState({ musics, favSongs });
+    // console.log(favSongs);
   }
 
   render() {
-    const { musics, getRespAPI } = this.state;
+    const { musics, getRespAPI, favSongs } = this.state;
     const validamusic = musics.length !== 0;
-    console.log(getRespAPI);
+    // console.log(getRespAPI);
 
     // console.log(musics);
     return (
@@ -32,7 +34,11 @@ class Album extends React.Component {
         {validamusic && <p data-testid="album-name">{musics[0].collectionName}</p> }
         {musics.filter((el, i) => i !== 0).map((music) => (
           <div key={ music.trackId + music.artistId }>
-            <MusicCard music={ music } />
+            <MusicCard
+              music={ music }
+              mussicFav={ (favSongs
+                .find((s) => s.trackId === music.trackId) !== undefined) }
+            />
           </div>
 
         ))}
